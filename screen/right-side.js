b1b0bc554bdd13b7c6d02e150775f8b2e9076086
Sunaez <kurdistan201324@gmail.com>
2023-09-18 13:10:28 +0100

    Finished up

--- screen/right-side.js
+++ screen/right-side.js
@@ -19,7 +19,7 @@ function rotateContent() {
 
         // Show the next div
         rotatingDivs[currentIndex].style.display = 'block';
-    }, 45 * 1000); // Rotate every x (* 1000) seconds
+    }, 30 * 1000); // Rotate every x (* 1000) seconds
 }
 
 // Initialize the rotation functionality
