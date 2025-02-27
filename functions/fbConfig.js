93f75f21a6b6198fed7e946dfd94ee8c5629bf52
Aland <kurdistan201324@gmail.com>
2025-02-27 20:44:54 +0000

    Unhide some keys

--- functions/fbConfig.js
+++ functions/fbConfig.js
@@ -2,13 +2,7 @@
 export async function onRequest(context) {
     // Read your secrets from the environment
     const config = {
-      apiKey: context.env.FB_API_KEY,
-      authDomain: context.env.FB_AUTH_DOMAIN,
-      projectId: context.env.FB_PROJECT_ID,
-      storageBucket: context.env.FB_STORAGE_BUCKET,
-      messagingSenderId: context.env.FB_MESSAGING_SENDER_ID,
-      appId: context.env.FB_APP_ID,
-      measurementId: context.env.FB_MEASUREMENT_ID
+      apiKey: context.env.FB_API_KEY
     };
   
     return new Response(JSON.stringify(config), {
