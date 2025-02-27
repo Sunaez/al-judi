d6f29109ddda7a54df87962c5d6a18472f306f28
Aland <kurdistan201324@gmail.com>
2025-02-27 20:54:14 +0000

    all the walls down

--- functions/fbConfig.js
+++ functions/fbConfig.js
@@ -1,12 +1,10 @@
-// /functions/fbConfig.js
-export async function onRequest(context) {
-    // Read your secrets from the environment
-    const config = {
-      apiKey: context.env.FB_API_KEY
-    };
-  
-    return new Response(JSON.stringify(config), {
-      headers: { "Content-Type": "application/json" }
-    });
-  }
-  
+// fbConfig.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyDkloxT5KTmy_CWe6FSsAYHykgdfoN_Gpo",
+  authDomain: "al-judimasjid.firebaseapp.com",
+  projectId: "al-judimasjid",
+  storageBucket: "al-judimasjid.firebasestorage.app",
+  messagingSenderId: "706961330765",
+  appId: "1:706961330765:web:cb3f6d734d41476f54c204",
+  measurementId: "G-MRKVDQ6PPG"
+};
